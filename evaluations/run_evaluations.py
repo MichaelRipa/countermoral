346b05e3ab1c0fcea7032ef9ca712953605ca13f
Michael Ripa <m.ripa123@gmail.com>
2024-06-16 17:20:56 +0000

    More cleanup: Cleaned up the argument parser, added more documentation and removed some unused variables

--- evaluations/run_evaluations.py
+++ evaluations/run_evaluations.py
@@ -18,7 +18,7 @@ from evaluations.utils.evaluation_utils import ike_few_shot, get_first_element, 
 from evaluations.utils.data_utils import unpack_data, unpack_data_bulk, prepare_portability_inputs, prepare_portability_inputs_bulk
 
 # Import EasyEdit dependencies
-from easyeditor import BaseEditor, ROMEHyperParams, FTHyperParams, IKEHyperParams, KNHyperParams, MEMITHyperParams, MENDHyperParams, GraceHyperParams, LoRAHyperParams, SERACHparams
+from easyeditor import BaseEditor, ROMEHyperParams, FTHyperParams, IKEHyperParams, KNHyperParams, MEMITHyperParams, MENDHyperParams, LoRAHyperParams, SERACHparams
 from easyeditor.evaluate import compute_edit_quality
 
 # Global variables
@@ -31,7 +31,6 @@ hparamClass = {
         'ike': IKEHyperParams,
         'memit': MEMITHyperParams,
         'mend': MENDHyperParams,
-        'grace': GraceHyperParams,
         'lora': LoRAHyperParams,
         'serac': SERACHparams,
 }
@@ -75,62 +74,8 @@ def generate_ike_prompts(editor, tokenizer : AutoTokenizer, request : dict, trai
     )
     return icl_examples
 
-#TODO: This should be refactored as much as possible
-def evaluate_entries_batch(dataset, model_type, hparams, edit_technique):
-
-    prompts, target_true, target_new, subject, action_paraphrased_prompts, relation_paraphrased_prompts, neighbourhood_prompts = unpack_data_bulk(dataset)
-
-    # Initialize new editor instance
-    global editor
-    editor = None
-    editor = BaseEditor.from_hparams(hparams)
-
-    target_true_broadcasted = [entry for entry in target_true for i in range(10)]
-
-    portability_inputs = prepare_portability_inputs_bulk(target_new, action_paraphrased_prompts, relation_paraphrased_prompts)
-
-    neighbourhood_scores_pre = get_probabilities(editor.model, tokenizer, neighbourhood_prompts, target_true_broadcasted)
-
-    metrics, edited_model, _ = editor.edit(
-            prompts=prompts,
-			ground_truth=target_true,
-			target_new=target_new,
-			subject=subject,
-			portability_inputs=portability_inputs,
-			keep_original_weights = False
-	)
-
-    print('Finished processing MEMIT!')
-    neighbourhood_scores_post = get_probabilities(editor.model, tokenizer, neighbourhood_prompts, target_true[0])
-    neighbourhood_scores = [abs(neighbourhood_scores_post[i] - neighbourhood_scores_pre[i]) for i in range(len(neighbourhood_scores_post))]
-
-    all_results = []
-
-    assert len(metrics) == len(dataset)
-
-    for i, entry in enumerate(metrics):
-        # This section is essentially the same as the code in `evaluate_entry()`, except that the neighbourhood scores need to be sliced from a larger list containing all the scores
-
-        reliability_score = entry['post']['rewrite_acc']
-        action_paraphrase_scores = [get_first_element(score[1]) for score in entry['post']['portability'].items() if 'action' in score[0]]
-        relation_paraphrase_scores = [get_first_element(score[1]) for score in entry['post']['portability'].items() if 'relation' in score[0]]
-        n_score = neighbourhood_scores[10*i:10*(i+1)]
-
-        result = {
-            'reliability': reliability_score,
-            'generalization_action_paraphrase': action_paraphrase_scores,
-            'generalization_relation_paraphrase': relation_paraphrase_scores,
-            'neighbourhood_score': n_score,
-            'meta_data': dataset[i]['meta_data']
-        }
-
-        print(result)
-        all_results.append(result)
-
-    return all_results
-
-
 def evaluate_entry(data_entry : dict, model_type : str, hparams, edit_technique : str) -> dict:
+    """Uses EasyEdit API to edit the base model with the provided edit template, and then evaluates the edit and returns the resulting metrics"""
 
     # Unpack the JSON object
     prompts, target_true, target_new, subject, action_paraphrased_prompts, relation_paraphrased_prompts, neighbourhood_prompts = unpack_data(data_entry)
@@ -148,6 +93,8 @@ def evaluate_entry(data_entry : dict, model_type : str, hparams, edit_technique 
 
     # Compute the pre-edited model accuracy on the neighbourhood prompts
     neighbourhood_scores_pre = get_probabilities(editor.model, tokenizer, neighbourhood_prompts, target_true[0])
+    neighbourhood_scores_post = []
+    neighbourhood_scores = []
 
     metrics, edited_model, _ = editor.edit(
             prompts=prompts,
@@ -162,20 +109,10 @@ def evaluate_entry(data_entry : dict, model_type : str, hparams, edit_technique 
     metrics = get_first_element(metrics)
 	
     reliability_score = get_first_element(metrics['post']['rewrite_acc'])
-    reliability_pre = get_first_element(metrics['pre']['rewrite_acc'])
-
-    # Might not keep this metric 
-    reliability_mag =  abs(reliability_pre - reliability_score)
-    print(metrics['post']['portability'])
-
     action_paraphrase_scores = [get_first_element(score[1]) for score in metrics['post']['portability'].items() if 'action' in score[0]]
     relation_paraphrase_scores = [get_first_element(score[1]) for score in metrics['post']['portability'].items() if 'relation' in score[0]]
 
-
-    print('Computing metric for data entry')
-    neighbourhood_scores_post = []
-    neighbourhood_scores = []
-
+    # Compute the neighbourhood score, by taking the difference in pre/post edit scores on out-of-distribution prompts
     if edit_technique == 'ike':
         # We create the in-context learning prompts for the requested rewrite, and then append each neighbourhood prompt to a copy for it to create "post-edit" evaluations
         # This way, we can evaluate locality on IKE (as other techniques evaluate an edited model on neighbourhood prompts directly)
@@ -189,7 +126,6 @@ def evaluate_entry(data_entry : dict, model_type : str, hparams, edit_technique 
         neighbourhood_scores_post = get_probabilities(editor.model, tokenizer, neighbourhood_prompts, target_true[0])
         neighbourhood_scores = [abs(neighbourhood_scores_post[i] - neighbourhood_scores_pre[i]) for i in range(len(neighbourhood_scores_post))]
 
-
     result = {
         'reliability': reliability_score,
         'generalization_action_paraphrase': action_paraphrase_scores,
@@ -197,8 +133,58 @@ def evaluate_entry(data_entry : dict, model_type : str, hparams, edit_technique 
         'neighbourhood_score': neighbourhood_scores,
         'meta_data': data_entry['meta_data']
     }
-    print(result)
     return result
+
+def evaluate_entries_batch(dataset : List[dict], model_type : str, hparams, edit_technique : str) -> List[dict]:
+    """Analagous to `evaluate_entries()`, except that it edits the model on a batch collection of entries before evaluating (used for MEMIT). Note that this is not analagous to just running `evaluate_entries()` repeatedly, as the base model gets edited on all examples before evaluations are run."""
+
+    prompts, target_true, target_new, subject, action_paraphrased_prompts, relation_paraphrased_prompts, neighbourhood_prompts = unpack_data_bulk(dataset)
+
+    # Initialize new editor instance
+    global editor
+    editor = None
+    editor = BaseEditor.from_hparams(hparams)
+
+    target_true_broadcasted = [entry for entry in target_true for i in range(10)]
+
+    portability_inputs = prepare_portability_inputs_bulk(target_new, action_paraphrased_prompts, relation_paraphrased_prompts)
+
+    neighbourhood_scores_pre = get_probabilities(editor.model, tokenizer, neighbourhood_prompts, target_true_broadcasted)
+
+    # Edit the base model on the entire dataset
+    metrics, edited_model, _ = editor.edit(
+            prompts=prompts,
+			ground_truth=target_true,
+			target_new=target_new,
+			subject=subject,
+			portability_inputs=portability_inputs,
+			keep_original_weights = False
+	)
+
+    neighbourhood_scores_post = get_probabilities(editor.model, tokenizer, neighbourhood_prompts, target_true[0])
+    neighbourhood_scores = [abs(neighbourhood_scores_post[i] - neighbourhood_scores_pre[i]) for i in range(len(neighbourhood_scores_post))]
+
+    assert len(metrics) == len(dataset)
+    all_results = []
+
+    for i, entry in enumerate(metrics):
+        # This section is essentially the same as the code in `evaluate_entry()`, except that the neighbourhood scores need to be sliced from a larger list containing all the scores
+        reliability_score = entry['post']['rewrite_acc']
+        action_paraphrase_scores = [get_first_element(score[1]) for score in entry['post']['portability'].items() if 'action' in score[0]]
+        relation_paraphrase_scores = [get_first_element(score[1]) for score in entry['post']['portability'].items() if 'relation' in score[0]]
+        n_score = neighbourhood_scores[10*i:10*(i+1)]
+
+        result = {
+            'reliability': reliability_score,
+            'generalization_action_paraphrase': action_paraphrase_scores,
+            'generalization_relation_paraphrase': relation_paraphrase_scores,
+            'neighbourhood_score': n_score,
+            'meta_data': dataset[i]['meta_data']
+        }
+        all_results.append(result)
+
+    return all_results
+
 
 def write_results(results : List[dict], ethical_framework : str, edit_technique : str, model :str, actions_broad : bool, model_type : str) -> None:
     """Writes evaluation results to a JSON file, whose path is specified by the edit technique, language model, portion of CounterMoral dataset and whether the evaluation was applied directly on the base-model, or to edited versions of it."""
@@ -211,7 +197,8 @@ def write_results(results : List[dict], ethical_framework : str, edit_technique 
     with open(output_path, 'w') as f:
         json.dump(results, f, indent=4)
 
-def run_evaluations(model, edit_technique, ethical_framework, actions_broad, model_type):
+def run_evaluations(model : str, edit_technique : str, ethical_framework : str, actions_broad : bool, model_type : str):
+    """Main function which both runs the evaluations and writes the results"""
     dataset = load_dataset(ethical_framework, actions_broad)
 
     if edit_technique != 'lora':
@@ -226,7 +213,7 @@ def run_evaluations(model, edit_technique, ethical_framework, actions_broad, mod
         tokenizer.add_special_tokens({'pad_token': '[PAD]'})
 
     results = []
-    if edit_technique in ['memit','grace']:
+    if edit_technique == 'memit':
         results = evaluate_entries_batch(dataset, model_type, hparams, edit_technique)
     else:
         for data_entry in dataset:
@@ -247,36 +234,29 @@ def main():
     parser.add_argument('--edit_technique', type=str, help='Editing technique to evaluate', required=is_required)
     parser.add_argument('--ethical_framework', type=str, required=is_required, choices=['CARE_ETHICS', 'DEONTOLOGY', 'UTILITARIANISM', 'VIRTUE_ETHICS'], help='Ethical framework to load the dataset for')
     parser.add_argument('--actions_broad', action='store_true', help='If set, loads the broad actions dataset (30 actions per framework) instead of the full dataset (300 examples per framework)')
-    parser.add_argument('--model_type', type=str, help='Specify whether to evaluate the base model or the edited model', choices=['base','edited'], required=is_required)
-    parser.add_argument('--easyeditor_path', type=str, help='Path to where the EasyEdit library is cloned', default = '/app/')
     parser.add_argument('--batch_evaluation', action='store_true',  help='If included, runs the batch edit techiques (MEMIT, GRACE) which evaluates model performance after having numerous edits applied at once.')
     
     args = parser.parse_args()
-    os.chdir(args.easyeditor_path)
+
     if args.all:
         # Run all possible combinations of evaluations
         model = args.model
         # Specify edit technique (if provided)
         edit_techniques = list(hparamClass.keys()) if args.edit_technique is None else [args.edit_technique]
         if not args.batch_evaluation:
-            if 'grace' in edit_techniques:
-                edit_techniques.remove('grace')
             if 'memit' in edit_techniques:
                 edit_techniques.remove('memit')
 
         ethical_frameworks = ["CARE_ETHICS", "DEONTOLOGY", "UTILITARIANISM", "VIRTUE_ETHICS"]
-        model_types = ['edited', 'base']
         actions_broad = [True, False]
         for use_broad_dataset in actions_broad:
             for edit_technique in edit_techniques:
                 for ethical_framework in ethical_frameworks:
-                    #for model_type in model_types:
                     if not check_evaluation_exists(edit_technique, model, ethical_framework, use_broad_dataset):
                         run_evaluations(model, edit_technique, ethical_framework, use_broad_dataset, 'edited')
     else:
         # Run a single evaluation
         run_evaluations(args.model, args.edit_technique, args.ethical_framework, args.actions_broad, 'edited')
-
 
 if __name__ == '__main__':
     seed_everything(42)
